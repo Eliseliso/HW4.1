b718c325757d8749292f53819bdf0f0bb5f64c0a
Elis <eliso1190@yandex.ru>
2023-01-30 13:47:32 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numberA = GetNum("Введите число A: ");
+int numberB = GetNum("Введите число B: ");
+Raise(numberA, numberB);
+void Raise(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(numberA + "->" + result);
+}
+int GetNum(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
